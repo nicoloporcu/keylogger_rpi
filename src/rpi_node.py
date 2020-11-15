2c138eac8b79420751230e8c63f232dcbedb1fc8
nicoloporcu <porcu@usc.edu>
2020-11-15 12:41:52 -0800

    started

---
+++ src/rpi_node.py
@@ -0,0 +1,35 @@
+import time
+import grovepi
+import numpy as np
+import paho.mqtt.client as mqtt
+import sys
+
+MAX_FRQ = 2000
+SLICE_SIZE = 0.15
+WINDOW_SIZE = 0.25 #seconds
+lock = threading.Lock()
+
+sound_sensor = 0
+grovepi.pinMode(sound_sensor, "INPUT")
+
+
+#y = []
+#y_filt = []
+#nyq_frq = MAX_FRQ/2
+
+
+if __name__ == '__main__':
+	
+	client = mqtt.Client()
+    client.connect(host="104.183.92.41:", port=11000, keepalive=60)
+    client.loop_start()
+
+
+    while True:
+
+    	with lock:
+    		x = np.linspace[1:MAX_FRQ/2]
+    		client.publish('data/unflitered', x)
+
+    	pass
+
